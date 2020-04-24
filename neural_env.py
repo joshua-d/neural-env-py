21eae76452205f1b6738c53506b26bed7358d662
Josh <joshdaley7@gmail.com>
2020-04-24 11:53:36 -0400

    backprop extension

--- neural_env.py
+++ neural_env.py
@@ -6,13 +6,13 @@ class NeuralEnv:
 
     class Neuron:
 
-        def __init__(self, layer, id, nbgvs = 0):
+        def __init__(self, layer, id):
             self.layer = layer
             self.id = id
             self.value = 0.5
             self.weights = []
             self.weighted = []
-            self.bias = rand.uniform(nbgvs * -1, nbgvs)
+            self.bias = 0
 
         def to_dict(self):
             neur_dict = {
@@ -51,28 +51,8 @@ class NeuralEnv:
             self.neuron = []
             self.nenv = nenv
 
-            self.subjectivity = rand.uniform(0, 1) + 1
-            self.ngs = rand.randint(0, nenv.output_neuron_amt)
-            self.nrs = rand.randint(0, nenv.output_neuron_amt)
-            self.wgs = rand.randint(0, nenv.output_neuron_amt)
-            self.wrs = rand.randint(0, nenv.output_neuron_amt)
-            self.nbgs = rand.randint(0, nenv.output_neuron_amt)
-            self.nbrs = rand.randint(0, nenv.output_neuron_amt)
-            self.wgvs = rand.uniform(0, 1)
-            self.nbgvs = rand.uniform(0, 1)
-            self.nbvs = rand.uniform(0, 1)
-            self.wvs = rand.uniform(0, 1)
-
-            self.fitness = 0
-
             for i in range(0, nenv.layer_amt):
                 self.neuron.append([])
-
-            for i in range(0, nenv.input_neuron_amt):
-                self.neuron[0].append(nenv.Neuron(0, i))
-
-            for i in range(0, nenv.output_neuron_amt):
-                self.neuron[nenv.output_layer].append(nenv.Neuron(nenv.output_layer, i))
 
         def input_data(self, input_list):
             for i in range(0, self.nenv.input_neuron_amt):
@@ -84,18 +64,12 @@ class NeuralEnv:
                     for weight in self.neuron[i][j].weights:
                         weight_total += weight.w_neuron.value * weight.value
                     weight_total += self.neuron[i][j].bias
-                    try:
-                        self.neuron[i][j].value = 1 / (1 + 2.718 ** (weight_total * (-1)))
-                    except OverflowError:
-                        if weight_total > 0:
-                            self.neuron[i][j].value = 1.0
-                        else:
-                            self.neuron[i][j].value = 0.0
-
-                    if self.neuron[i][j].value < self.nenv.neuron_floor:
-                        self.neuron[i][j].value = 0
-                    elif self.neuron[i][j].value > self.nenv.neuron_cap:
+                    if weight_total > 15:
                         self.neuron[i][j].value = 1
+                    elif weight_total < -15:
+                        self.neuron[i][j].value = 0
+                    else:
+                        self.neuron[i][j].value = 1 / (1 + 2.718 ** (weight_total * (-1)))
 
         def add_weight(self, neuron, w_neuron, value):
             for weight in neuron.weights:
@@ -135,7 +109,9 @@ class NeuralEnv:
             while len(self.neuron[layer]) == self.nenv.max_h_neuron_amt:
                 layer = rand.randint(1, self.nenv.max_hidden_layer_amt)
 
-            self.neuron[layer].append(self.nenv.Neuron(layer, len(self.neuron[layer]), self.nbgvs))
+            neur = self.nenv.Neuron(layer, len(self.neuron[layer]))
+            neur.bias = rand.uniform(self.nbgvs * -1, self.nbgvs)
+            self.neuron[layer].append(neur)
 
         def remove_neuron(self):
             if len(self.get_neuron_list(1, self.nenv.output_layer)) == 0:
@@ -193,20 +169,25 @@ class NeuralEnv:
             return self.neuron[self.nenv.output_layer]
         
         def to_dict(self):
-            net_dict = {
-                'subjectivity': self.subjectivity,
-                'ngs': self.ngs,
-                'nrs': self.nrs,
-                'wgs': self.wgs,
-                'wrs': self.wrs,
-                'nbgs': self.nbgs,
-                'nbrs': self.nbrs,
-                'wgvs': self.wgvs,
-                'nbgvs': self.nbgvs,
-                'nbvs': self.nbgvs,
-                'wvs': self.wvs,
-                'neuron': []
-            }
+            if self.nenv.learning_style == 'NEAT':
+                net_dict = {
+                    'subjectivity': self.subjectivity,
+                    'ngs': self.ngs,
+                    'nrs': self.nrs,
+                    'wgs': self.wgs,
+                    'wrs': self.wrs,
+                    'nbgs': self.nbgs,
+                    'nbrs': self.nbrs,
+                    'wgvs': self.wgvs,
+                    'nbgvs': self.nbgvs,
+                    'nbvs': self.nbgvs,
+                    'wvs': self.wvs,
+                    'neuron': []
+                }
+            elif self.nenv.learning_style == 'backpropagation':
+                net_dict = {
+                    'neuron': []
+                }
 
             for i in range(self.nenv.layer_amt):
                 net_dict['neuron'].append([])
@@ -215,32 +196,97 @@ class NeuralEnv:
 
             return net_dict
 
-    def __init__(self, pop_size, input_neuron_amt, max_h_neuron_amt, output_neuron_amt, max_hidden_layer_amt, fitness_function=0):
-        self.pop_size = pop_size
-        self.input_neuron_amt = input_neuron_amt
-        self.max_h_neuron_amt = max_h_neuron_amt
-        self.output_neuron_amt = output_neuron_amt
-        self.max_hidden_layer_amt = max_hidden_layer_amt
-        if fitness_function == 0:
-            self.fitness_function = self.evaluate_fitnesses
+
+    def __init__(self, config):
+
+        if 'learning_style' in config:
+            
+            if config['learning_style'] == 'NEAT':
+                
+                self.learning_style = 'NEAT'
+
+                if 'population_size' in config:
+                    self.pop_size = config['pop_size']
+                else:
+                    self.pop_size = 100
+
+                if 'input_neuron_amount' in config:
+                    self.input_neuron_amt = config['input_neuron_amount']
+                else:
+                    pass  # exception
+
+                if 'max_hidden_neuron_amount' in config:
+                    self.max_h_neuron_amt = config['max_hidden_neuron_amount']
+                else:
+                    self.max_h_neuron_amt = 0
+
+                if 'max_hidden_layer_amount' in config:
+                    self.max_hidden_layer_amt = config['max_hidden_layer_amount']
+                else:
+                    self.max_hidden_layer_amt = 0
+                    
+                if 'output_neuron_amount' in config:
+                    self.output_neuron_amt = config['output_neuron_amount']
+                else:
+                    pass  # exception
+
+                if 'fitness_function' in config:
+                    self.fitness_function = config['fitness_function']
+                else:
+                    self.fitness_function = self.evaluate_fitnesses_neat
+
+                self.cost_degree = 1
+
+                self.layer_amt = self.max_hidden_layer_amt + 2
+                self.output_layer = self.max_hidden_layer_amt + 1
+
+                self.networks = []
+                self.create_networks()
+
+
+            elif config['learning_style'] == 'backpropagation':
+
+                self.learning_style = 'backpropagation'
+
+                if 'neurons' in config:
+                    self.neurons = config['neurons']
+                else:
+                    pass  # exception
+
+                self.initial_weight_range = 15
+                self.initial_bias_range = 15
+
+                self.max_weight_nudge_amt = 1
+                self.max_bias_nudge_amt = 1
+                self.max_aim_nudge_amt = 1
+
+                self.input_neuron_amt = self.neurons[0]
+                self.output_neuron_amt = self.neurons[len(self.neurons) - 1]
+                self.layer_amt = len(self.neurons)
+                self.output_layer = len(self.neurons) - 1
+
+                self.network = self.Network(self)
+
+
+        if 'training_data' in config:
+            training_data = config['training_data']
+            self.inputs = []
+            self.desired_outputs = []
+            for datum in training_data:
+                if len(datum) != 2 or datum[0] != self.input_neuron_amt or datum[1] != self.output_neuron_amt:
+                    pass  # exception
+                self.inputs.append(datum[0])
+                self.desired_outputs.append(datum[1])
         else:
-            self.fitness_function = fitness_function
+            self.inputs = []
+            self.desired_outputs = []
 
-        self.cost_degree = 1
 
-        self.layer_amt = max_hidden_layer_amt + 2
-        self.output_layer = max_hidden_layer_amt + 1
-        self.inputs = []
-        self.desired_outputs = []
+        self.weight_cap = 15
+        self.weight_floor = -15
+        self.bias_cap = 15
+        self.bias_floor = -15
 
-        self.networks = []
-
-        self.neuron_cap = 0.999999
-        self.neuron_floor = 0.000001
-        self.weight_cap = 10
-        self.weight_floor = -10
-
-        self.create_networks()
         self.initialize_networks()
 
     def create_networks(self):
@@ -248,18 +294,55 @@ class NeuralEnv:
             self.networks.append(self.Network(self))
 
     def initialize_networks(self):
-        for network in self.networks:
-            for i in range(0, self.output_neuron_amt):
-                if rand.randint(0, self.output_neuron_amt) < network.wgs:
-                    network.generate_weight()
-                if rand.randint(0, self.output_neuron_amt) < network.nbgs:
-                    self.networks[i].generate_bias()
+        if self.learning_style == 'NEAT':
+            for network in self.networks:
 
-    def add_input_output(self, input_list, output_list):
+                network.subjectivity = rand.uniform(0, 1) + 1
+                network.ngs = rand.randint(0, self.output_neuron_amt)
+                network.nrs = rand.randint(0, self.output_neuron_amt)
+                network.wgs = rand.randint(0, self.output_neuron_amt)
+                network.wrs = rand.randint(0, self.output_neuron_amt)
+                network.nbgs = rand.randint(0, self.output_neuron_amt)
+                network.nbrs = rand.randint(0, self.output_neuron_amt)
+                network.wgvs = rand.uniform(0, 1)
+                network.nbgvs = rand.uniform(0, 1)
+                network.nbvs = rand.uniform(0, 1)
+                network.wvs = rand.uniform(0, 1)
+
+                network.fitness = 0
+
+                for i in range(0, self.input_neuron_amt):
+                    network.neuron[0].append(self.Neuron(0, i))
+
+                for i in range(0, self.output_neuron_amt):
+                    network.neuron[self.output_layer].append(self.Neuron(self.output_layer, i))
+
+                for i in range(0, self.output_neuron_amt):
+                    if rand.randint(0, self.output_neuron_amt) < network.wgs:
+                        network.generate_weight()
+                    if rand.randint(0, self.output_neuron_amt) < network.nbgs:
+                        self.networks[i].generate_bias()
+
+        elif self.learning_style == 'backpropagation':
+
+            self.network.fitness = 0
+
+            for i in range(self.layer_amt):
+                for j in range(self.neurons[i]):
+                    neuron = self.Neuron(i, j)
+                    neuron.bias = rand.uniform(-self.initial_bias_range, self.initial_bias_range)
+                    if i != 0:
+                        for w_neur in self.network.neuron[i-1]:
+                            value = rand.uniform(-self.initial_weight_range, self.initial_weight_range)
+                            self.network.add_weight(neuron, w_neur, value)
+                    self.network.neuron[i].append(neuron)
+
+
+    def add_training_data(self, input_list, output_list):
         self.inputs.append(input_list)
         self.desired_outputs.append(output_list)
 
-    def evaluate_fitnesses(self, nenv):
+    def evaluate_fitnesses_neat(self, nenv):
         if len(self.inputs) != 0:
             for network in self.networks:
                 network.fitness = 0
@@ -317,7 +400,13 @@ class NeuralEnv:
         for i in range(1, self.layer_amt):
             for j in range(0, len(parent.neuron[i])):
                 child.neuron[i].append(self.Neuron(i, j))
+
                 child.neuron[i][j].bias = parent.neuron[i][j].bias + rand.uniform(parent.nbvs * -1, parent.nbvs)
+                if child.neuron[i][j].bias > self.bias_cap:
+                    child.neuron[i][j].bias = self.bias_cap
+                elif child.neuron[i][j].bias < self.bias_floor:
+                    child.neuron[i][j].bias = self.bias_floor
+
                 for weight in parent.neuron[i][j].weights:
                     weight_value = weight.value + rand.uniform(parent.wvs * -1, parent.wvs)
                     if weight_value < self.weight_floor:
@@ -434,6 +523,126 @@ class NeuralEnv:
     def get_best_network(self):
         return self.networks[0]
 
+
+    # Backpropagation
+
+    def evaluate_fitness_backpropagation(self):
+        if len(self.inputs) == 0:
+            pass  # exception
+        else:
+            self.network.fitness = 0
+            for i in range(0, len(self.inputs)):
+                self.network.input_data(self.inputs[i])
+                for j in range(0, self.output_neuron_amt):
+                    self.network.fitness += (self.desired_outputs[i][j] - self.network.neuron[self.output_layer][j].value) ** 2
+
+    def train(self):
+
+        wb_nudge_table = {}
+
+        # Loop through layers from output to first hidden
+        j = self.output_layer
+        while j > 0:
+
+            aim_table = {}
+
+            # For each input
+            for i in range(len(self.inputs)):
+                input_list = self.inputs[i]
+                desired_output_list = self.desired_outputs[i]
+                self.network.input_data(input_list)
+
+                # output neuron aim values into aim table
+                if j == self.output_layer:
+                    for k in range(self.output_neuron_amt):
+                        aim_table[self.network.get_output_neurons()[k]] = desired_output_list[k]
+
+                for neuron in self.network.neuron[j]:
+
+                    # if neuron has no aim, no need to change weights/biases
+                    if neuron not in aim_table:
+                        continue
+
+                    # get desired value from aim table
+                    desired_value = aim_table[neuron]
+                    cost = desired_value - neuron.value
+                    if cost == 0:
+                        continue
+
+
+                    # nudge each weight relative to activation and cost in direction of cost
+                    for weight in neuron.weights:
+                        w_nudge = self.max_weight_nudge_amt * weight.w_neuron.value * cost
+
+                        if weight in wb_nudge_table:
+                            wb_nudge_table[weight].append(w_nudge)
+                        else:
+                            wb_nudge_table[weight] = [w_nudge]
+                            
+
+                        # if weighted neuron is in input layer, or the weight is 0, no need to nudge the aim
+                        if weight.w_neuron.layer == 0 or weight.value == 0:
+                            continue
+
+                        # nudge aim value relative to weight value and cost in direction of weight
+                        a_nudge = self.max_aim_nudge_amt * (weight.value / self.weight_cap) * abs(cost)  # TODO doesn't work if cap and floor are different
+                        if weight.w_neuron in aim_table:
+                            aim_table[weight.w_neuron].append(a_nudge)
+                        else:
+                            aim_table[weight.w_neuron] = [a_nudge]
+
+
+                    # nudge bias relative to cost in direction of cost TODO this may be wrong method
+                    b_nudge = self.max_bias_nudge_amt * cost
+                    if neuron in wb_nudge_table:
+                        wb_nudge_table[neuron].append(b_nudge)
+                    else:
+                        wb_nudge_table[neuron] = [b_nudge]
+
+
+            # done with layer; set aims of previous layer
+            for key in aim_table:
+                neuron = key
+                if isinstance(aim_table[neuron], list):
+                    total = 0
+                    for nudge in aim_table[neuron]:
+                        total += nudge
+                    aim_table[neuron] = total / len(aim_table[neuron])
+                    if aim_table[neuron] > 1:
+                        aim_table[neuron] = 1
+                    elif aim_table[neuron] < 0:
+                        aim_table[neuron] = 0
+
+            j -= 1
+
+
+        # done with all layers; all nudges collected
+        for key in wb_nudge_table:
+            # nudge weights
+            if isinstance(key, self.Weight):
+                weight = key
+                total = 0
+                for nudge in wb_nudge_table[weight]:
+                    total += nudge
+                weight.value += total / len(wb_nudge_table[weight])
+                if weight.value > self.weight_cap:
+                    weight.value = self.weight_cap
+                elif weight.value < self.weight_floor:
+                    weight.value = self.weight_floor
+            # nudge biases
+            elif isinstance(key, self.Neuron):
+                neuron = key
+                total = 0
+                for nudge in wb_nudge_table[neuron]:
+                    total += nudge
+                neuron.bias += total / len(wb_nudge_table[neuron])
+                if neuron.bias > self.bias_cap:
+                    neuron.bias = self.bias_cap
+                elif neuron.bias < self.bias_floor:
+                    neuron.bias = self.bias_floor
+
+    # IO
+
     def export_network(self, network, file_name):
         net_dict = network.to_dict()
         w_file = open(file_name, "w")
@@ -447,19 +656,24 @@ class NeuralEnv:
         net_dict = json.loads(r_file.read())
         r_file.close()
 
-        network = self.Network(self)
+        if self.learning_style == 'NEAT':
+            network = self.Network(self)
 
-        network.subjectivity = net_dict['subjectivity']
-        network.ngs = net_dict['ngs']
-        network.nrs = net_dict['nrs']
-        network.wgs = net_dict['wgs']
-        network.wrs = net_dict['wrs']
-        network.nbgs = net_dict['nbgs']
-        network.nbrs = net_dict['nbrs']
-        network.wgvs = net_dict['wgvs']
-        network.nbgvs = net_dict['nbgvs']
-        network.nbvs = net_dict['nbvs']
-        network.wvs = net_dict['wvs']
+            network.subjectivity = net_dict['subjectivity']
+            network.ngs = net_dict['ngs']
+            network.nrs = net_dict['nrs']
+            network.wgs = net_dict['wgs']
+            network.wrs = net_dict['wrs']
+            network.nbgs = net_dict['nbgs']
+            network.nbrs = net_dict['nbrs']
+            network.wgvs = net_dict['wgvs']
+            network.nbgvs = net_dict['nbgvs']
+            network.nbvs = net_dict['nbvs']
+            network.wvs = net_dict['wvs']
+
+        elif self.learning_style == 'backpropagation':
+            network = self.network
+            self.initialize_networks()
 
         for i in range(self.layer_amt):
             for neur_dict in net_dict['neuron'][i]:
